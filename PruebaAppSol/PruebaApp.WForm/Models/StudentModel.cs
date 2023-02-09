802ebaec9af3dcc3e0d013ab463bedb752cf777c
MatosWagner22 <62521625+MatosWagner22@users.noreply.github.com>
2023-02-09 16:35:52 -0400

    Instructor model

---
+++ PruebaAppSol/PruebaApp.WForm/Models/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaApp.WForm.Models
+{
+    internal class StudentModel
+    {
+    }
+}
